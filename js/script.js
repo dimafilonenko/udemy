7649595f0bbfa1810a40b0823f5df93bfb5ff4a3
Dmytro <filonenko.d.v.twin@gmail.com>
2020-10-29 20:43:12 +0200

    six commit

--- js/script.js
+++ js/script.js
@@ -11,12 +11,29 @@ let personalMovieDB = {
   privat: false
 };
 
-const a = prompt("Один из последних просмотреных фильмов?", " ");
-const b = prompt("Насколько оцените его?", " ");
-const c = prompt("Один из последних просмотреных фильмов?", " ");
-const d = prompt("Насколько оцените его?", " ");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++ ) {
+  const a = prompt("Один из последних просмотреных фильмов?", " ");
+  const b = prompt("Насколько оцените его?", " ");
+
+  if(a != null && b != null && a != " " && b != " " && a.length < 50){
+    personalMovieDB.movies[a] = b;
+  }else{
+    i--;
+  }
+  
+}
+if (personalMovieDB.count < 10 ){
+  console.log("Просмотрено довольно мало фильмов");
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+  console.log("Вы классический зритель");
+}else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
 
 console.log(personalMovieDB);
+

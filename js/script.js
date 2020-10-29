313e98af2e436a1eaad316f6673a9ed1a4c26b27
Dmytro <filonenko.d.v.twin@gmail.com>
2020-10-29 17:18:35 +0200

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 "use strict";
 
-console.log(1);
-console.log(2);
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", 10);
+console.log(numberOfFilms);
 
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt("Один из последних просмотреных фильмов?", " ");
+const b = prompt("Насколько оцените его?", " ");
+const c = prompt("Один из последних просмотреных фильмов?", " ");
+const d = prompt("Насколько оцените его?", " ");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
